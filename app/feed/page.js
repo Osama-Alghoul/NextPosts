b0115594e3f04b950daf86cb266715f613f7dc98
OsssGhoul <osamalghoul2@gmail.com>
2025-09-29 16:32:38 +0300

    Enhancing Images and added metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,13 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+export async function generateMetadate() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+  title: `Borwser our ${numberOfPosts} Posts`,
+  description: "See all our posts !",
+}};
 
 export default async function FeedPage() {
   const posts = await getPosts();
